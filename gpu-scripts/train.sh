4a364c9cb6de7b35ab133c160926b2d10e307c0c
Nauxe <51425614+Nauxe@users.noreply.github.com>
2025-10-27 16:33:31 +0800

    Add gpu scripts based on soc-cluster.sh

---
+++ gpu-scripts/train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J d3dg 
+#SBATCH --gres=gpu:a100-40:1
+
+#SBATCH --time=05:00:00 # Time limit (hh:mm:ss)
+#SBATCH --partition=gpu
+#SBATCH --mem=32G
+#SBATCH --output="slurm-out/train-%j.out"
+
+#SBATCH --mail-type=ALL 
+#SBATCH --mail-user=$SLURM_EMAIL
+
+source ~/.bashrc 
+
+conda activate 3dg
+
+python train.py
---
+++ gpu-scripts/visualize.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J d3dg 
+#SBATCH --gres=gpu:a100-40:1
+
+#SBATCH --time=05:00:00 # Time limit (hh:mm:ss)
+#SBATCH --partition=gpu
+#SBATCH --mem=32G
+#SBATCH --output="slurm-out/visualize-%j.out"
+
+#SBATCH --mail-type=ALL 
+#SBATCH --mail-user=$SLURM_EMAIL
+
+source ~/.bashrc 
+
+conda activate 3dg 
+
+python visualize.py
+
